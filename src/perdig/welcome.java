71e8ad9366b622c266e97c6ad7c22121464dc186
Adzraftkha <Adzraftkha@github.com>
2020-05-15 23:51:04 +0800

    Tugas Akhir

---
+++ src/perdig/welcome.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perdig;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author user
+ */
+public class welcome {
+    
+    
+public static void main(String[]args){
+JOptionPane.showMessageDialog(null, "Selamat datang di Perpustakaan Digital");
+new Login().setVisible(true);
+}
+}
